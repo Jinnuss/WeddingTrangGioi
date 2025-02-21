d0fbccd55768e30a9baa0146c14f6aadf6e7702a
Nguyễn Thành Trung <b28082002@gmail.com>
2025-02-21 22:42:28 +0700

    album anh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { useState } from 'react';
 import './App.css';
-import chure from './imgs/chure.jpg';
-import codau from './imgs/codau.jpg';
-import couple from './imgs/couple.jpg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { IoIosCloseCircleOutline } from "react-icons/io";
@@ -10,43 +7,43 @@ import "flatpickr/dist/flatpickr.css";
 import Flatpickr from "react-flatpickr";
 import cap1 from './imgs2/cap1.jpg';
 import cap2 from './imgs2/cap2.jpg';
-import cap3 from './imgs2/cap3.jpg';
-import cap4 from './imgs2/cap4.jpg';
-import cap5 from './imgs2/cap5.jpg';
-import cap6 from './imgs2/cap6.jpg';
+// import cap3 from './imgs2/cap3.jpg';
+// import cap4 from './imgs2/cap4.jpg';
+// import cap5 from './imgs2/cap5.jpg';
+// import cap6 from './imgs2/cap6.jpg';
 import cap7 from './imgs2/cap7.jpg';
 import cap8 from './imgs2/cap8.jpg';
 import cap9 from './imgs2/cap9.jpg';
-import cap10 from './imgs2/cap10.jpg';
-import cap11 from './imgs2/cap11.jpg';
-import cap12 from './imgs2/cap12.jpg';
-import cap13 from './imgs2/cap13.jpg';
-import cap14 from './imgs2/cap14.jpg';
-import cap15 from './imgs2/cap15.jpg';
-import cap16 from './imgs2/cap16.jpg';
-import cap17 from './imgs2/cap17.jpg';
+// import cap10 from './imgs2/cap10.jpg';
+// import cap11 from './imgs2/cap11.jpg';
+// import cap12 from './imgs2/cap12.jpg';
+// import cap13 from './imgs2/cap13.jpg';
+// import cap14 from './imgs2/cap14.jpg';
+// import cap15 from './imgs2/cap15.jpg';
+// import cap16 from './imgs2/cap16.jpg';
+// import cap17 from './imgs2/cap17.jpg';
 import cap18 from './imgs2/cap18.jpg';
 import cap19 from './imgs2/cap19.jpg';
 import cap20 from './imgs2/cap20.jpg';
-import cap21 from './imgs2/cap21.jpg';
-import cap22 from './imgs2/cap22.jpg';
-import cap23 from './imgs2/cap23.jpg';
+// import cap21 from './imgs2/cap21.jpg';
+// import cap22 from './imgs2/cap22.jpg';
+// import cap23 from './imgs2/cap23.jpg';
 import cap24 from './imgs2/cap24.jpg';
 import cap25 from './imgs2/cap25.jpg';
 import cap26 from './imgs2/cap26.jpg';
-import cap27 from './imgs2/cap27.jpg';
-import cap28 from './imgs2/cap28.jpg';
-import cap29 from './imgs2/cap29.jpg';
-import cap30 from './imgs2/cap30.jpg';
-import cap31 from './imgs2/cap31.jpg';
-import cap32 from './imgs2/cap32.jpg';
+// import cap27 from './imgs2/cap27.jpg';
+// import cap28 from './imgs2/cap28.jpg';
+// import cap29 from './imgs2/cap29.jpg';
+// import cap30 from './imgs2/cap30.jpg';
+// import cap31 from './imgs2/cap31.jpg';
+// import cap32 from './imgs2/cap32.jpg';
 import cap33 from './imgs2/cap33.jpg';
-import codau1 from './imgs2/codau1.jpg';
-import codau2 from './imgs2/codau2.jpg';
-import codau3 from './imgs2/codau3.jpg';
-import codau4 from './imgs2/codau4.jpg';
+// import codau1 from './imgs2/codau1.jpg';
+// import codau2 from './imgs2/codau2.jpg';
+// import codau3 from './imgs2/codau3.jpg';
+// import codau4 from './imgs2/codau4.jpg';
 import codau5 from './imgs2/codau5.jpg';
-import chure1 from './imgs2/chure1.jpg';
+// import chure1 from './imgs2/chure1.jpg';
 import chure2 from './imgs2/chure2.jpg';
 
 AOS.init();
@@ -209,7 +206,7 @@ function App() {
                   </div>
                 </div>
                 <div className="img w-[285px] sm:h-[320px]   overflow-hidden imgshadow">
-                  <img src={couple} alt="ảnh 1" className="object-cover " />
+                  <img src={cap1} alt="ảnh 1" className="object-cover " />
                 </div>
               </div>
             </div>
@@ -228,7 +225,39 @@ function App() {
           <div className="font1 text-[42px] lg:text-[60px] mt-[50px] mb-[30px]">
             Album Ảnh Cưới
           </div>
+          <div className="flex justify-center mt-[30px] mb-[30px] md:mb-[130px]">
+            <div className="w-[44%] md:w-[285px] md:h-[320px] ml-[10px]" >
+              <img src={cap2} alt="ảnh 1" className="object-cover img2" />
+            </div>
+            <div className="w-[50%] ">
+              <div className="w-[42%] flex md:w-[150px] md:h-[320px] mr-[10px] ml-[10px] mb-[10px] sm:mb-[0px]" >
+                <img src={cap8} alt="ảnh 1" className="object-cover img2 mr-[10px]" />
+                <img src={cap18} alt="ảnh 1" className="object-cover img2" />
+              </div>
+              <div className="w-[42%] flex md:w-[150px] md:h-[320px] mr-[10px] ml-[10px]">
+                <img src={cap9} alt="ảnh 1" className="object-cover img2  mr-[10px]" />
+                <img src={cap19} alt="ảnh 1" className="object-cover img2" />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center mt-[30px] mb-[30px] md:mb-[130px]">
+
+            <div className="w-[50%] ">
+              <div className="w-[42%] flex md:w-[150px] md:h-[320px] mr-[10px] ml-[10px] mb-[10px] sm:mb-[0px]" >
+                <img src={cap24} alt="ảnh 1" className="object-cover img2 mr-[10px]" />
+                <img src={cap7} alt="ảnh 1" className="object-cover img2" />
+              </div>
+              <div className="w-[42%] flex md:w-[150px] md:h-[320px] mr-[10px] ml-[10px]">
+                <img src={cap25} alt="ảnh 1" className="object-cover img2  mr-[10px]" />
+                <img src={cap26} alt="ảnh 1" className="object-cover img2" />
+              </div>
+            </div>
+            <div className="w-[44%] md:w-[285px] md:h-[320px] ml-[10px]" >
+              <img src={cap33} alt="ảnh 1" className="object-cover img2" />
+            </div>
+          </div>
         </div>
+
 
 
 
